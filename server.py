3b8895001eb7a4135805342e97f758106e3ac728
James Percival <jpercival@unomaha.edu>
2015-03-26 14:44:23 -0500

    Commiting my changes before pulling

--- server.py
+++ server.py
@@ -147,6 +147,12 @@ def admin():
     if current_user.is_admin:
         return render_template('admin.html', form=AddRoomForm())
 
+@login_required
+@app.route('/classSelector',methods=['GET'])
+def class_selector():
+    return render_template('classSelector.html')
+
+
 ##Actions
 @login_required
 @app.route('/user/add', methods=['POST'])
